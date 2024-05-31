40b87bd74a92a8100d0ecdd971d5cba1fdf978d5
Anirudh Pappu <apappu@umd.edu>
2024-05-31 13:54:04 -0500

    Made changes

--- tree.py
+++ tree.py
@@ -3,13 +3,13 @@ from collections import Counter
 
 
 class Node:
-    def __init__(self, feature=None, threshold=None, left=None, right=None, *, value=None):
+    def __init__(self, feature=None, threshold=None, left=None, right=None,*,value=None):
         self.feature = feature
         self.threshold = threshold
         self.left = left
         self.right = right
-        self.value = None
-
+        self.value = value
+        
     def is_leaf(self):
         # Tells that this is a leaf node since only leaf nodes have non-null values
         return self.value is not None
@@ -22,40 +22,43 @@ class DecisionTree:
         self.num_features = num_features
         self.root = None
 
-    def fit(self, x, y):
-        self.num_features = x.shape[1] if not self.num_features else min(
-            x.shape[1], self.num_features)
-        self.root = self._grow_tree(x, y)
+    def fit(self, X, y):
+        self.num_features = X.shape[1] if not self.num_features else min(
+            X.shape[1], self.num_features)
+        self.root = self._grow_tree(X, y)
 
-    def _grow_tree(self, x, y, depth=0):
-        num_samples, n_feats = x.shape
+    def _grow_tree(self, X, y, depth=0):
+        num_samples, n_feats = X.shape
         num_labels = len(np.unique(y))
-        # check stopping criteria
+
+        # check the stopping criteria
         if (depth >= self.max_depth or num_labels == 1 or num_samples < self.min_sample_split):
             leaf_value = self._most_common_label(y)
             return Node(value=leaf_value)
 
-        features_idx = np.random.choice(
-            n_feats, self.num_features, replace=False)
+        features_idx = np.random.choice(n_feats, self.num_features, replace=False)
 
-        # find best split
-        best_feature, best_threshold = self._best_split(x, y, features_idx)
+        # find the best split
+        best_feature, best_threshold = self._best_split(X, y, features_idx)
 
-        # create child node
-        left_idxs, right_idxs = self._split(x[:, best_feature], best_threshold)
-        left = self._grow_tree(x[left_idxs, :], y[left_idxs], depth+1)
-        right = self._grow_tree(x[right_idxs, :], y[right_idxs], depth+1)
+        # create child nodes
+        left_idxs, right_idxs = self._split(X[:, best_feature], best_threshold)
+        left = self._grow_tree(X[left_idxs, :], y[left_idxs], depth+1)
+        right = self._grow_tree(X[right_idxs, :], y[right_idxs], depth+1)
         return Node(best_feature, best_threshold, left, right)
 
-    def _best_split(self, x, y, feats_idx):
+
+    def _best_split(self, X, y, feat_idxs):
         best_gain = -1
         split_idx, split_threshold = None, None
-        for feat_idx, in feats_idx:
-            x_col = x[:, feat_idx]
-            thresholds = np.unique(x_col)
+
+        for feat_idx in feat_idxs:
+            X_col = X[:, feat_idx]
+            thresholds = np.unique(X_col)
+
             for thr in thresholds:
                 # calculate the information gain
-                gain = self._information_gain(y, x_col, thr)
+                gain = self._information_gain(y, X_col, thr)
 
                 # if the best gain is lower than current gain, we reassign the new value to best_gain
                 if gain > best_gain:
@@ -65,31 +68,29 @@ class DecisionTree:
 
         return split_idx, split_threshold
 
-    def _information_gain(self, y, x_col, thr):
-        # Information gained is the entropy of the parent - [weighted avg]*entropy of children
 
+    def _information_gain(self, y, X_col, threshold):
         # parent entropy
         parent_entropy = self._entropy(y)
+        
         # create children
-        left_idx, right_idx = self._split(x_col, thr)
-        if len(left_idx) == 0 or len(right_idx) == 0:
+        left_idxs, right_idxs = self._split(X_col, threshold)
+        if len(left_idxs) == 0 or len(right_idxs) == 0:
             return 0
-        # calculate weighted entropy of children
+        
+        # calculate the weighted avg. entropy of children
         n = len(y)
-        n_left, n_right = len(left_idx), len(right_idx)
-        entropy_left, entropy_right = self.entropy(
-            y[left_idx]), self.entropy(y[right_idx])
-        child_entropy = ((n_left/n) * entropy_left)+((n_right/n)*entropy_right)
+        n_left, n_right = len(left_idxs), len(right_idxs)
+        entropy_left, entropy_right = self._entropy(y[left_idxs]), self._entropy(y[right_idxs])
+        child_entropy = ((n_left/n) * entropy_left) + ((n_right/n) * entropy_right)
 
-        # calc information gained
+        # calculate the IG
         information_gain = parent_entropy - child_entropy
         return information_gain
 
-    # creates children
-
-    def _split(self, x_col, split_thresh):
-        left_idxs = np.argwhere(x_col <= split_thresh).flatten()
-        right_idxs = np.argwhere(x_col >= split_thresh).flatten()
+    def _split(self, X_col, split_thresh):
+        left_idxs = np.argwhere(X_col <= split_thresh).flatten()
+        right_idxs = np.argwhere(X_col > split_thresh).flatten()
         return left_idxs, right_idxs
 
     # calculates entropy of given node
@@ -105,5 +106,13 @@ class DecisionTree:
         value = counter.most_common(1)[0][0]
         return value
 
-    def predict(self, x):
-        return np.array([self._traverse_tree(val, self.root) for val in x])
+    def predict(self, X):
+        return np.array([self._traverse_tree(x, self.root) for x in X])
+
+    def _traverse_tree(self, x, node):
+        if node.is_leaf():
+            return node.value
+
+        if x[node.feature] <= node.threshold:
+            return self._traverse_tree(x, node.left)
+        return self._traverse_tree(x, node.right)
